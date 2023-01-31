5ed92f0f0d72ff11af13fc4105d8f0d578f64df5
ausias <ausias@hINFO6-8.xiam.cat>
2023-01-31 13:06:35 +0100

    pol

--- Uf3-Practica1/src/uf3/practica1/Uf3Practica1Ex1.java
+++ Uf3-Practica1/src/uf3/practica1/Uf3Practica1Ex1.java
@@ -9,20 +9,32 @@ import java.io.*;
 
 public class Uf3Practica1Ex1 {
 
-    public static void LlegirFrase() {
+    static Scanner scan = new Scanner(System.in);
+    static final String FITXER = "./textos.txt";
 
+    public static void main(boolean crearF, String escribir, String esborra) throws IOException {
+        File txt = new File(FITXER);
+        FileWriter fileW = new FileWriter(txt, crearF);
+        PrintWriter printW = new PrintWriter(fileW);
+
+        String missatge = scan.nextLine();
+
+        while (!missatge.equals(escribir)) {
+            if (missatge.equals(esborra)) {
+                fileW.close();
+                fileW = new FileWriter(txt, false);
+                printW = new PrintWriter(fileW);
+            } else {
+                printW.write(missatge + "\n");
+            }
+            missatge = scan.nextLine();
+        }
+
+        fileW.flush();
+        fileW.close();
     }
 
-    public static void main(String[] args) {
-        File textos = new File("./textos.txt");
-        FileWriter writer = new FileWriter(textos, true);
-        PrintWriter pw = new PrintWriter(writer);
-
-        pw.println("Hola");
-
-        pw.flush();
-
-        writer.close();
+    static void nuevaLinea() throws IOException {
+        insertarLinea(false, "", null);
     }
-
 }
